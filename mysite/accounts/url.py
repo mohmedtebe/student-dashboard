5078100cd0dc3ed0e1811ec34923938ef6ab9127
mohmedtebe <mohmedtebe@yahoo.com>
2022-02-10 17:44:04 +0200

    dashboard

---
+++ mysite/accounts/url.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+
+    path('signup/', views.signup, name="signup"),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('settings/change_password', auth_views.PasswordChangeView.as_view(template_name='change_password.html'),
+         name='change_password'),
+    path('settings/change_password/done', auth_views.PasswordChangeDoneView.as_view(template_name='password_change_done.html'),
+         name='password_change_done'),
+    path('account/', views.UserUpdateView.as_view(), name='my_account'),
+]
